480129069d41729a1022f00b754e7a26ae5c8002
Rebeca Albuquerque <rebeca.albuquerque96@gmail.com>
2018-08-05 22:49:56 -0300

    Adição da BaseActivity

---
+++ app/src/main/java/br/com/albuquerque/agendatelefonicanati/modules/contacts/view/BaseActivity.kt
@@ -0,0 +1,47 @@
+package br.com.albuquerque.agendatelefonicanati.modules.contacts.view
+
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import br.com.albuquerque.agendatelefonicanati.R
+
+abstract class BaseActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun showPopup(context: Context, title: String, msg: String, positiveAction: () -> Unit, negativeAction: (() -> Unit)?){
+        AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(msg)
+                .setPositiveButton(getString(R.string.popup_yes), DialogInterface.OnClickListener { dialog, id ->
+                    positiveAction()
+                })
+                .setNegativeButton(getString(R.string.popup_no), DialogInterface.OnClickListener { dialog, id ->
+                    negativeAction?.let {
+                        it()
+                    }
+                    dialog.dismiss()
+                })
+                .create()
+                .show()
+    }
+
+}
